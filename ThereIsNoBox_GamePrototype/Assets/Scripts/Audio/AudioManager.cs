a83a696331e2a2fd381b6fb16fabe328d24b0209
jrworthington <james@jamesworthington.net>
2026-07-01 23:49:42 +0100

    Second commit adding sound to Room 1 with temp character for example

---
+++ ThereIsNoBox_GamePrototype/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager in the scene.");
+
+    }
+    instance = this;
+        }
+}
---
+++ ThereIsNoBox_GamePrototype/Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header];
+}
