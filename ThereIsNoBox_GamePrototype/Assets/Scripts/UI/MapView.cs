a8834900ff885ee6585a4d761a71c8798da88f02
DraconianOne <draconian1@gmail.com>
2026-07-02 15:19:38 +0100

    Added basic character UI
    Added basic character UI. Updated Map screen and controllers.

--- ThereIsNoBox_GamePrototype/Assets/Scripts/UI/MapView.cs
+++ ThereIsNoBox_GamePrototype/Assets/Scripts/UI/MapView.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 public class MapView: MonoBehaviour
 {
- 
+    
+    public bool IsActive => base.gameObject.activeSelf;
+    
     public void ToggleView()
     {
-        Game.HUD.ToggleHUD(gameObject.activeSelf);
-        gameObject.SetActive(!gameObject.activeSelf);
+        //Game.HUD.ToggleHUD(gameObject.activeSelf);
+        base.gameObject.SetActive(!gameObject.activeSelf);
     }
     
 }
