aa36e37b205acb3bd2de0387907240754e047eb5
DraconianOne <draconian1@gmail.com>
2026-07-03 10:06:55 +0100

    Added stuff

---
+++ ThereIsNoBox_GamePrototype/Assets/Scripts/Rooms/RoomDef.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+    
+[CreateAssetMenu(fileName = "Room_", menuName = "Back In Your Box/Room Config", order = 0)]
+public class RoomDef: ScriptableObject
+{
+        public string RoomName;
+        public string RoomDescription;
+        
+        
+}
